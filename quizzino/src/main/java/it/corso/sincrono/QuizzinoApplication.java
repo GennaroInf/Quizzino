2fb9a8290fe5ffa31e1e76f687d4db05bf40de94
Utente <Utente@DESKTOP-MJC5OMH>
2019-06-27 16:41:00 +0200

    Progetto Base

---
+++ quizzino/src/main/java/it/corso/sincrono/QuizzinoApplication.java
@@ -0,0 +1,13 @@
+package it.corso.sincrono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzinoApplication.class, args);
+	}
+
+}
